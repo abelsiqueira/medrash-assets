8230a92ac696b605a2c49dfbb1d8c77a3ca3e3c6
Abel Siqueira <abel.s.siqueira@gmail.com>
2012-06-30 11:31:46 -0300

    Add OnlyPlay.cs

---
+++ Scripts/OnlyPlay.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class OnlyPlay : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		animation.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animation.Play();
+	}
+}
