466c88893a48f222a5e18ef79b8897adf3639355
Tiago Cinto <tiagocinto@gmail.com>
2012-07-03 17:06:13 -0300

    Implementacao da nova animacao de walk para o Merdash caminhar na agua

--- Characters/Medrash/Scripts/MainCharacter.cs
+++ Characters/Medrash/Scripts/MainCharacter.cs
@@ -20,7 +20,7 @@ public class MainCharacter : MonoBehaviour
 	private float positionX;
 	private float positionZ;
 	
-	private float energyLossValue = 0.5f;
+	private float energyLossValue = 0.1f;
 	private float lifeLossValue = 0.1f;
 	private float temperatureLossValue = 0.2f;
 	private float torchTimerValue = 10.0f;
